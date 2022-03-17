954e1c8d14ed976d5a82002c3e4f705b252caf54
dawnzmn <83068262+dawnzmn@users.noreply.github.com>
2022-03-17 17:00:24 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+import { Text, View } from 'react-native';
+import BottomTab from "./components/bottomTabNavigator";
+export default class App extends Component{
+  render(){
+    return ( 
+        <BottomTab/>
+    );
+  }
+}
+
